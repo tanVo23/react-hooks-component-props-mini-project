9af0ef1b7afc795fd2894c9fccfdde79d7b2624c
Tan <tan10tvo@gmail.com>
2021-12-08 11:10:30 -0700

    commit tree 5c55ebf62ed1138263e11d4ff66474cad7dbd7bb

---
+++ src/components/ArticleList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({posts}){
+    return(
+        <main>
+           {posts.map((posts) => <Article 
+           key={posts.id}
+           title={posts.title}
+           date={posts.date}
+           preview={posts.preview}/>
+           )}
+        </main>
+    );
+}
+
+export default ArticleList
